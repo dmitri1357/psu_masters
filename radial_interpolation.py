ebf6efc045e8a629c666dacd75fee3b8aa5b52d8
Dmitri Kalashnikov <48308024+dmitri1357@users.noreply.github.com>
2019-09-21 11:16:43 -0700

    Update radial_interpolation.py

--- radial_interpolation.py
+++ radial_interpolation.py
@@ -14,61 +14,47 @@ import geopy.distance as gd
 import scipy.interpolate as si
 
 
-def define_input_grid(array_latmin, array_latmax, array_latstep,
-                  array_lonmin, array_lonmax, array_lonstep):
-    """
-    array_latmin = southern latitude extent of input data array
-    array_latmax = northern latitude extent of input data array
-    array_latstep = latitudinal resolution (in degrees) of input data array
-
-    array_lonmin = western longitude extent of input data array
-    array_lonmax = eastern longitude extent of input data array
-    array_lonstep = longitudinal resolution (in degrees) of input data array
-    """
-
-    array_lats = np.arange(array_latmin,array_latmax+1,array_latstep)
-    array_lons = np.arange(array_lonmin,array_lonmax+1,array_lonstep)
-
-    return array_lats, array_lons
-
-
-def build_radial_grid(center_lat, center_lon, start_dist,
-                      dist_step, end_dist, degree_resolution):
-    """
-    center_lat = central latitude point around which the unit circle interpolator will be built
-    center_lon = central longitude point around which the unit circle interpolator will be built
-
-    start_dist = radius distance in km (from central point) of first (innermost) interpolation ring
-    dist_step = radius distance in km between each interpolation ring
-    end_dist = radius distance in km (from central point) of last (outermost) interpolation ring;
-               this distance defines the radius of the circle and the outer edge of
-               interpolated values relative to the origin latitude/longitude point (e.g. 2500 km)
-
-    degree_resolution = spacing, in degrees, between each interpolation point on interpolation rings
-    """
-
-    radial_increments = np.arange(start_dist,end_dist+1,dist_step)
-    azimuth_increments = np.arange(0+degree_resolution,360+1,degree_resolution)
-    latvec = []
-    lonvec = []
-
-    for km in radial_increments:
-        for deg in azimuth_increments:
-            start = geopy.Point(center_lat,center_lon)
-            circ = gd.distance(kilometers = km)
-            dest = circ.destination(point = start, bearing = deg)
-            latvec.append(dest[0])
-            lonvec.append(dest[1])
-
-    return latvec, lonvec
-
-
-def radial_interp(array, array_lats, array_lons, latvec, lonvec):
+def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_centerlon,
+                  start_radius, radius_step, end_radius, degree_resolution):
     """
     array = 2D input array of values from which to interpolate (data should be spatial
             and continuous on a regular grid, e.g. raster, reanalysis, climate model output, etc)
+    array_lats = vector of latitude values defining input data array
+    array_lons = vector of longitude values defining input data array
+    interp_centerlat = latitude defining origin around which the unit circle interpolator
+                       will be built
+    interp_centerlon = longitude defining origin around which the unit circle interpolator
+                       will be built
+    start_radius = radius distance in km (from origin) of first (innermost) interpolation ring
+    radius_step = radius distance in km between each interpolation ring
+    end_radius = radius distance in km (from origin) of last (outermost) interpolation ring;
+               this distance defines the radius of the full circle and the outer edge of
+               interpolated values relative to the origin latitude/longitude point (e.g. 2500 km)
+    degree_resolution = spacing, in degrees, between each interpolation point on unit circle.
+                        For example, specifying a value of 1 will yield 360 interpolation points
+                        on each interpolation ring.
+
+
+    *** Returns vector of interpolated values drawn from input array at every lat,lon point on
+        radial grid
     """
 
-    interp_vals = si.interpn((array_lons,array_lats),array,(lonvec,latvec))
+    radius_increments = np.arange(start_radius,end_radius+1,radius_step)
+    degree_increments = np.arange(0+degree_resolution,360+1,degree_resolution)
+    interp_lats = []
+    interp_lons = []
+
+    for km in radius_increments:
+        for deg in degree_increments:
+            start = geopy.Point(interp_centerlat,interp_centerlon)
+            transect = gd.distance(kilometers = km)
+            dest = transect.destination(point = start, bearing = deg)
+            interp_lats.append(dest[0])
+            interp_lons.append(dest[1])
+
+    interp_vals = si.interpn((array_lons,array_lats),array,(interp_lons,interp_lats))
     return interp_vals
+
+
+
 
