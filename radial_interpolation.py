2a9b0f31d0af2f64b0aa48ec445777a2ec39378d
Dmitri Kalashnikov <48308024+dmitri1357@users.noreply.github.com>
2019-09-23 09:52:25 -0700

    Update radial_interpolation.py

--- radial_interpolation.py
+++ radial_interpolation.py
@@ -14,17 +14,8 @@ import geopy.distance as gd
 import scipy.interpolate as si
 
 
-def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_centerlon,
-                  start_radius, radius_step, end_radius, degree_resolution):
+def define_radial_grid(start_radius, radius_step, end_radius, degree_resolution):
     """
-    array = 2D input array of values from which to interpolate (data should be spatial
-            and continuous on a regular grid, e.g. raster, reanalysis, climate model output, etc)
-    array_lats = vector of latitude values defining input data array
-    array_lons = vector of longitude values defining input data array
-    interp_centerlat = latitude defining origin around which the unit circle interpolator
-                       will be built
-    interp_centerlon = longitude defining origin around which the unit circle interpolator
-                       will be built
     start_radius = radius distance in km (from origin) of first (innermost) interpolation ring.
                    If start_radius = 0, interpolation at origin and at 0 degree azimuth
                    will be duplicated in order to produce a continous contourf plot for visualization.
@@ -32,7 +23,7 @@ def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_center
                    0 degree azimuth will be interpolated only once. Use this version if computing
                    spatial statistics as all interpolation points will be unique. Use version
                    with start_radius = 0 for plotting, as otherwise the plot will show no values
-                   around center and in a vertical slice at 0 degrees from center.
+                   around center and no values in a vertical slice at 0 degrees from center.
     radius_step = radius distance in km between each interpolation ring
     end_radius = radius distance in km (from origin) of last (outermost) interpolation ring;
                this distance defines the radius of the full circle and the outer edge of
@@ -42,16 +33,49 @@ def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_center
                         on each interpolation ring.
 
 
-    *** Returns vector of interpolated values drawn from input array at every lat,lon point on
-        radial grid
+    *** Returns tuple of polar coordinates (kilometers, degrees) defining the radial
+        interpolation grid in geographic space
     """
 
     if start_radius == 0:
 
         radius_increments = np.arange(start_radius,end_radius+1,radius_step)
         degree_increments = np.arange(0,360+1,degree_resolution)
-        interp_lats = []
-        interp_lons = []
+        return radius_increments, degree_increments
+
+    elif start_radius > 0:
+
+        radius_increments = np.arange(start_radius,end_radius+1,radius_step)
+        degree_increments = np.arange(0+degree_resolution,360+1,degree_resolution)
+        return radius_increments, degree_increments
+
+    else:
+        print('radius must be positive value')
+
+
+def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_centerlon,
+                  radius_increments, degree_increments):
+    """
+    array = 2D input array of values from which to interpolate (data should be spatial
+            and continuous on a regular grid, e.g. raster, reanalysis, climate model output, etc)
+    array_lats = vector of latitude values defining input data array
+    array_lons = vector of longitude values defining input data array
+    interp_centerlat = latitude defining origin around which the unit circle interpolator
+                       will be built
+    interp_centerlon = longitude defining origin around which the unit circle interpolator
+                       will be built
+    radius_increments = distance (km) between interpolation rings
+    degree_increments = azimuth resolution (degrees) between interpolation points
+
+
+    *** Returns vector of interpolated values drawn from input array at every lat,lon point on
+        radial grid
+    """
+
+    interp_lats = []
+    interp_lons = []
+
+    if radius_increments[0] == 0:
 
         for km in radius_increments:
             for deg in degree_increments:
@@ -61,12 +85,11 @@ def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_center
                 interp_lats.append(dest[0])
                 interp_lons.append(dest[1])
 
-    elif start_radius > 0:
+        interp_vals = si.interpn((array_lons,array_lats),array,(interp_lons,interp_lats))
+        del interp_lats, interp_lons
+        return interp_vals
 
-        radius_increments = np.arange(start_radius,end_radius+1,radius_step)
-        degree_increments = np.arange(0+degree_resolution,360+1,degree_resolution)
-        interp_lats = []
-        interp_lons = []
+    elif radius_increments[0] > 0:
 
         for km in radius_increments:
             for deg in degree_increments:
@@ -78,11 +101,13 @@ def radial_interp(array, array_lats, array_lons, interp_centerlat, interp_center
         interp_lats = np.hstack([interp_centerlat, interp_lats])
         interp_lons = np.hstack([interp_centerlon, interp_lons])
 
-    else:
-        print('radius must be positive value')
+        interp_vals = si.interpn((array_lons,array_lats),array,(interp_lons,interp_lats))
+        del interp_lats, interp_lons
+        return interp_vals
 
-    interp_vals = si.interpn((array_lons,array_lats),array,(interp_lons,interp_lats))
-    return interp_vals
+    else:
+        del interp_lats, interp_lons
+        print('radius must be positive value')
 
 
 
